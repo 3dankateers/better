13c8b1ee022df8b0681c56a62b422d395d637aac
Diablo117 <Diablo117OOS@Gmail.com>
2017-10-30 16:24:44 -0400

    added rate limiter and split into a few functions

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 """
 
 """
-import zlib
-import zmq
-import simplejson
+#Used to change json to python dicts for easier reading
 import json
+#Gets the json file and headers from html
 import urllib
+#For passing parameters into the program later and the emergency close in rate limiter
 import sys
-
 import sqlite3
+
+
 conn = sqlite3.connect('data/League.sqlite')
 c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS test2 (summonerID, playerName, role, season, champion, lane);")
+c.execute("CREATE TABLE IF NOT EXISTS test3 (summonerID, role, season, champion, lane);")
 conn.commit()
 
-
+#Gunna need to update this everyday, fun
 API_KEY = "RGAPI-ee771521-37c9-4c27-b826-2e6ae442a45c"
 
 CHALLENGER_LEAGUE = "https://na1.api.riotgames.com/lol/league/v3/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=" + API_KEY
@@ -24,22 +25,18 @@ def main():
 	url = CHALLENGER_LEAGUE
 
 	response = urllib.urlopen(url)
-	data = simplejson.loads(response.read())
+	dontgetbanned(response)
+	data = json.loads(response.read())
 	entries = data["entries"]
 	for rows in range (len(entries)):
 		summonerID = entries[rows]['playerOrTeamId']
 		#Replace name with a questionmark if it cant be encoded, koreans playing this game reeeeee
-		playerName = entries[rows]['playerOrTeamName'].encode(sys.stdout.encoding, errors = 'replace')
+		#This is actually useless, not gunna put it into db because I dont think its worth fixing all the potential errors with people using random characters
+		#and sqlite doenst like all types of encoding
+		#######playerName = entries[rows]['playerOrTeamName'].encode(sys.stdout.encoding, errors = 'replace')
 
 		#PlayerID to accountID conversion
-		SUMMONER_V3 = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/" + summonerID + "?api_key=" + API_KEY
-
-		url = SUMMONER_V3
-		response = urllib.urlopen(url)
-		data = simplejson.loads(response.read())
-		summonerdata = data
-
-		playerID = summonerdata["accountId"]
+		playerID = summonerID_TOaccountID(summonerID)
 		#print playerID
 
 		#After you hvae the ID's get their match history
@@ -47,10 +44,10 @@ def main():
 
 		url = ACCOUNT_MATCH_LIST
 		response = urllib.urlopen(url)
-		data = simplejson.loads(response.read())
+		dontgetbanned(response)
+		data = json.loads(response.read())
 		matches = data["matches"]
 
-		#print matches[0]['role']
 
 
 		for rows in range (len(matches)):
@@ -58,18 +55,43 @@ def main():
 			season = matches[rows]['season']
 			champion = matches[rows]['champion']
 			lane = matches[rows]['lane']
-			
+
 			#shove data into sqlite
-			c.execute("INSERT INTO test2 VALUES (?,?,?,?,?,?);", (summonerID, playerName, role, season, champion, lane))
+			c.execute("INSERT INTO test3 VALUES (?,?,?,?,?);", (summonerID, role, season, champion, lane))
 			conn.commit()
-			#print role
-			#print season
-			#print champion
-			#print lane
+
+
 
 	#close connection to sqlite
 	f.close();
 	conn.close();
+
+#Returns account ID from a known summoner ID
+def summonerID_TOaccountID(summonerID):
+	SUMMONER_V3 = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/" + summonerID + "?api_key=" + API_KEY
+	url = SUMMONER_V3
+	response = urllib.urlopen(url)
+	dontgetbanned(response)
+	data = json.loads(response.read())
+	summonerdata = data
+	playerID = summonerdata["accountId"]
+	return playerID
+
+#Rate limiter, stops the program if a rate goes above the league rate
+#Should make it pause instead of stop, too lazy atm
+def dontgetbanned(response):
+	#Gets data from league API headers, contains limit and how much youve used
+	curAppCount = response.info().getheader('X-App-Rate-Limit-Count')
+	curAppLimit = response.info().getheader('X-App-Rate-Limit')
+	x = curAppCount.split(',')
+	RequestsPerSecond = x[1].split(':')
+	x = curAppLimit.split(',')
+	MaxRequestsPerSecond = x[1].split(':')
+	if(int(RequestsPerSecond[0])>int(MaxRequestsPerSecond[0])):
+		sys.exit("Rate too high")
+
+
+
 
 
 if __name__ == '__main__':
