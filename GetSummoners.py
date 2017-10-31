49c63f5075d5a438869ef7ab436b74aa5eeba061
Diablo117 <Diablo117OOS@Gmail.com>
2017-10-31 18:13:59 -0400

    GetSummoners is superior version of getchallengers

--- GetSummoners.py
+++ GetSummoners.py
@@ -11,31 +11,25 @@ import sqlite3
 #waiting
 import time
 
-conn = sqlite3.connect('data/Challengers.sqlite')
+conn = sqlite3.connect('data/Summoners.sqlite')
 c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS ChallengerLeague (summonerID, accountID);")
+c.execute("CREATE TABLE IF NOT EXISTS SummonersList (summonerID, accountID, tier, region, rank, leaguePoints, queueType, dateScraped);")
 conn.commit()
 
 #Gunna need to update this everyday, fun
-API_KEY = "RGAPI-ee771521-37c9-4c27-b826-2e6ae442a45c"
+API_KEY = "RGAPI-e52906c9-7b3b-4128-873b-056798b39877"
 
 CHALLENGER_LEAGUE = "https://na1.api.riotgames.com/lol/league/v3/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=" + API_KEY
+MASTERS_LEAGUE = "https://na1.api.riotgames.com/lol/league/v3/masterleagues/by-queue/RANKED_SOLO_5x5?api_key=" + API_KEY
 
 def main():
-	#Get summoner IDs from challenger league
-	url = CHALLENGER_LEAGUE
 
-	response = urllib.urlopen(url)
-	dontgetbanned(response)
-	data = json.loads(response.read())
+	#use this if you want some other league
+	LEAGUE_UUID = ""
+	RANDOM_LEAGUE = "https://na1.api.riotgames.com/lol/league/v3/leagues/" + LEAGUE_UUID +"?api_key=" + API_KEY
 
-	entries = data["entries"]
-	for rows in range (len(entries)):
-		summonerID = entries[rows]['playerOrTeamId']
-		playerID = summonerID_TOaccountID(summonerID)
-		c.execute("INSERT INTO ChallengerLeague VALUES (?,?);", (summonerID, playerID))
-		conn.commit()
-		logging("Row " + str(rows) + " inserted into Challenger.sqlite")
+	insertIntoDB(pullSummonersFromURL(CHALLENGER_LEAGUE))
+	#insertIntoDB(pullSummonersFromURL(MASTERS_LEAGUE))
 
 	#close connection to sqlite
 	conn.close();
@@ -50,7 +44,6 @@ def summonerID_TOaccountID(summonerID):
 		data = json.loads(response.read())
 		summonerdata = data
 		playerID = summonerdata["accountId"]
-		time.sleep(1)
 		return playerID
 	except KeyError, e:
 		dump_to_file(summonerdata)
@@ -71,9 +64,10 @@ def dontgetbanned(response):
 	MaxRequestsPerSecond = x[1].split(':')
 	MaxRequestsPerMinute = x[0].split(':')
 
-	if(int(RequestsPerMinute[0])>(int(MaxRequestsPerMinute[0])-2)):
+	#its actually per 2 minutes
+	if(int(RequestsPerMinute[0])>(int(MaxRequestsPerMinute[0])-3)):
 		print "Rate too high"
-		time.sleep(30)
+		time.sleep(110)
 
 	if(int(RequestsPerSecond[0])>(int(MaxRequestsPerSecond[0])-2)):
 		print "Rate too high"
@@ -89,6 +83,51 @@ def logging(insert):
 	logFile = open("log.txt","a")
 	logFile.write(insert + "\n")
 	logFile.close()
+
+def pullSummonersFromURL(url):
+	summonerID = []
+	playerID = []
+	league = []
+	regionList = []
+	rank = []
+	leaguePoints = []
+	queueList = []
+
+	region = "NOT DETECTED"
+	queueType = "NOT DETECTED"
+
+	response = urllib.urlopen(url)
+	dontgetbanned(response)
+	data = json.loads(response.read())
+
+	if "na1" in url:
+		region = "NA1"
+
+	if "RANKED_SOLO_5x5" in url:
+		queueType = "RANKED SOLO 5x5"
+
+
+
+	entries = data["entries"]
+	for rows in range (len(entries)):
+		summonerID.append(entries[rows]['playerOrTeamId'])
+		playerID.append(summonerID_TOaccountID(summonerID[rows]))
+		league.append(data["tier"])
+		regionList.append(region)
+		rank.append(entries[rows]['rank'])
+		leaguePoints.append(entries[rows]['leaguePoints'])
+		queueList.append(queueType)
+
+
+
+	#combine lists into a tuple
+	databaseInsert = zip(summonerID,playerID,league,regionList,rank,leaguePoints,queueList)
+	return databaseInsert
+
+def insertIntoDB(tupleInsert):
+	c.executemany("INSERT OR IGNORE INTO SummonersList VALUES (?,?,?,?,?,?,?,datetime('now','localtime'));", (tupleInsert))
+	conn.commit()
+	conn.close()
 
 if __name__ == '__main__':
 	main()
